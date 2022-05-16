4da451d4402d89d2e9bb18aea5834cdb0b3def24
Nkanyiso Ntshangase <nkanyisontshangase349@gmail.com>
2022-05-16 11:12:51 +0200

    added auth code for logout and logging functionality

--- app.js
+++ app.js
@@ -27,13 +27,66 @@ class Note {
       this.$closeModalForm = document.querySelector("#modal-btn");
       this.$sidebar = document.querySelector(".sidebar");
       this.$sidebarActiveItem = document.querySelector(".active-item");
+
+     
+    this.$app = document.querySelector("#app");
+    this.$firebaseAuthContainer = document.querySelector(
+      "#firebaseui-auth-container"
+    );
+    this.$authUserText = document.querySelector(".auth-user");
+    this.$logoutBtn = document.querySelector(".logout");
+
+    this.ui = new firebaseui.auth.AuthUI(auth);
+    this.handleAuth();
+
+  
       this.addEventListeners();
       this.render();
+    }
+   
+  
+    handleAuth() {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+          this.$authUserText.innerHTML = user.displayName;
+          this.redirectToApp();
+        } else {
+          this.redirectToAuth();
+        }
+      });
+    }
+  
+    handleLogout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.redirectToAuth();
+        })
+        .catch((error) => {
+          console.log("ERROR OCCURED", error);
+        });
+    }
+  
+    redirectToApp() {
+      this.$firebaseAuthContainer.style.display = "none";
+      this.$app.style.display = "block";
+    }
+  
+    redirectToAuth() {
+      this.$firebaseAuthContainer.style.display = "block";
+      this.$app.style.display = "none";
+  
+      this.ui.start("#firebaseui-auth-container", {
+        signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
+        // Other config options...
+      });
+
     }
     addEventListeners(){
         document.body.addEventListener("click", (event) => {
             this.handleFormClick(event);
-          this.closeModal(event);
+            this.closeModal(event);
             this.openModal(event);
             this.handleArchiving(event);
         });
@@ -54,6 +107,9 @@ class Note {
         });
         this.$sidebar.addEventListener("mouseout", (event) => {
           this.handleToggleSidebar(event);
+        });
+        this.$logoutBtn.addEventListener("click", (event) => {
+          this.handleLogout();
         });
              
     }
